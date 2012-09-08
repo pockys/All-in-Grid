2e8ee736b36bf5a5e65bf03f94b29070e60cdedd
Donghee Kim <dhkim@port4art.live-on.net>
2012-09-08 13:49:15 +0900

    add contact menu

--- All_in_Grid/src/org/pockys/allingrid/MainActivity.java
+++ All_in_Grid/src/org/pockys/allingrid/MainActivity.java
@@ -4,21 +4,24 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.GridView;
+import android.widget.HorizontalScrollView;
 
 public class MainActivity extends Activity {
 
 	static final String TAG = "MainActivity";
-	private GridView gridView;
+	private GridView gridField;
+	private HorizontalScrollView mainField;
+	
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		gridView = (GridView) findViewById(R.id.grid);
-		gridView.setAdapter(new ContactAdapter(this));
+		gridField = (GridView) findViewById(R.id.grid_field);
+		gridField.setAdapter(new ContactAdapter(this));
+		
+		mainField = (HorizontalScrollView)findViewById(R.id.menu_field);
 
-		
-		
 	}
 
 	public boolean onCreateOptionsMenu(Menu menu) {
